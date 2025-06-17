9750d11a1b11b24080233b79a641eb9f727e1ba0
Matt Mmo <mmattmmo@gmail.com>
2025-06-17 11:39:43 +0200

    Update to the new SCLS version

--- sources/video_sort.cpp
+++ sources/video_sort.cpp
@@ -12,7 +12,7 @@ void bubble_video(std::string path, int datas_number, int sort_width, int width,
     std::shared_ptr<pleos::Sort_Datas> sort_datas = pleos::algorithms_sort_creation(datas_number);
     scls::Video_Encoder video = scls::Video_Encoder(path, (static_cast<double>(datas_number) / speed + 1.0) / 60.0, width, height);
     while(video.frame_count() > video.current_frame()){
-        std::shared_ptr<scls::Image> current_image = std::make_shared<scls::Image>(width, height, scls::Color(255, 255, 255));
+        std::shared_ptr<scls::__Image_Base> current_image = std::make_shared<scls::__Image_Base>(width, height, scls::Color(255, 255, 255));
         current_image.get()->paste(pleos::algorithms_sort_image(current_image, sort_datas.get(), width / 2 - sort_width / 2, 100, sort_width, sort_width).get(), 0, 0);
         video.write_video_frame(current_image);
         video.go_to_next_frame();
@@ -30,7 +30,7 @@ void fusion_video(std::string path, int datas_number, int sort_width, int width,
     std::shared_ptr<pleos::Sort_Datas> sort_datas = pleos::algorithms_sort_creation(datas_number);
     scls::Video_Encoder video = scls::Video_Encoder(path, (static_cast<double>(algorithms_comparaison_fusion_time(sort_datas.get())) / speed + 1.0) / 60.0, width, height);
     while(video.frame_count() > video.current_frame()){
-        std::shared_ptr<scls::Image> current_image = std::make_shared<scls::Image>(width, height, scls::Color(255, 255, 255));
+        std::shared_ptr<scls::__Image_Base> current_image = std::make_shared<scls::__Image_Base>(width, height, scls::Color(255, 255, 255));
         current_image.get()->paste(pleos::algorithms_sort_image(current_image, sort_datas.get(), width / 2 - sort_width / 2, 100, sort_width, sort_width).get(), 0, 0);
         video.write_video_frame(current_image);
         video.go_to_next_frame();
@@ -48,7 +48,7 @@ void insertion_video(std::string path, int datas_number, int sort_width, int wid
     std::shared_ptr<pleos::Sort_Datas> sort_datas = pleos::algorithms_sort_creation(datas_number);
     scls::Video_Encoder video = scls::Video_Encoder(path, (static_cast<double>(datas_number) / speed + 1.0) / 60.0, width, height);
     while(video.frame_count() > video.current_frame()){
-        std::shared_ptr<scls::Image> current_image = std::make_shared<scls::Image>(width, height, scls::Color(255, 255, 255));
+        std::shared_ptr<scls::__Image_Base> current_image = std::make_shared<scls::__Image_Base>(width, height, scls::Color(255, 255, 255));
         current_image.get()->paste(pleos::algorithms_sort_image(current_image, sort_datas.get(), width / 2 - sort_width / 2, 100, sort_width, sort_width).get(), 0, 0);
         video.write_video_frame(current_image);
         video.go_to_next_frame();
@@ -66,7 +66,7 @@ void selection_video(std::string path, int datas_number, int sort_width, int wid
     std::shared_ptr<pleos::Sort_Datas> sort_datas = pleos::algorithms_sort_creation(datas_number);
     scls::Video_Encoder video = scls::Video_Encoder(path, (static_cast<double>(datas_number) / speed + 1.0) / 60.0, width, height);
     while(video.frame_count() > video.current_frame()){
-        std::shared_ptr<scls::Image> current_image = std::make_shared<scls::Image>(width, height, scls::Color(255, 255, 255));
+        std::shared_ptr<scls::__Image_Base> current_image = std::make_shared<scls::__Image_Base>(width, height, scls::Color(255, 255, 255));
         int sort_width = 600;
         current_image.get()->paste(pleos::algorithms_sort_image(current_image, sort_datas.get(), width / 2 - sort_width / 2, 100, sort_width, sort_width).get(), 0, 0);
         video.write_video_frame(current_image);
